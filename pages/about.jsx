57404ed31acf298271c51514b8bffb7c4208b60c
RaggedyRagz <JohnArigo96706@gmail.com>
2022-10-13 06:52:07 -0700

    created nextjs app

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const about = () => {
+  return (
+    <div>
+      <Meta title="about" />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
